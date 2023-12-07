3ea9f6f61be86420dde5796c722f3d2fd4c1c057
TaeHoon0 <kimtahwn@naver.com>
2023-12-07 12:01:07 +0900

    merge

--- restraunt.py
+++ restraunt.py
@@ -24,11 +24,15 @@ class Restaurants(db.Model):
 with app.app_context():
     db.create_all()
 
+@app.route('/home')
+def home():
+    return render_template('main.html')
+
 @app.route('/') #리스트 출력
 def index():
     restaurant_list = Restaurants.query.all()
 
-    return render_template('restraunt.html', restaurant_list = restaurant_list)
+    return render_template('restraunt-1.html', restaurant_list = restaurant_list)
 
 @app.route('/api/foodie', methods=['POST']) #식당 추가
 def foodie_create():
@@ -39,17 +43,20 @@ def foodie_create():
         address_receive = request.form['address']
         style_receive = request.form['style']
         review_receive = request.form['review']
-        img_receive = request.form['img']
+        uploaded_file = request.files['img']
+        img_name = uploaded_file.filename
+        uploaded_file.save("./static/upload/"+img_name+".jpeg")
+
     except SQLAlchemyError as e:
         flash("오류가 발생했습니다.")
 
     restaurant = Restaurants(userid = userid_receive, username = username_receive, shopname = shopname_receive, 
-                            address =  address_receive, style  = style_receive, review = review_receive, img = img_receive)
+                            address =  address_receive, style  = style_receive, review = review_receive, img = img_name)
     db.session.add(restaurant)
     db.session.commit()
     
-    return render_template('restraunt.html')
-    #return redirect(url_for('index.html'))
+    #return render_template('restraunt.html')
+    return redirect(url_for('index'))
 
 @app.route('/api/foodie/', methods=['POST']) 
 #@app.route('/api/foodie/<id>', methods=['post']) 삭제
